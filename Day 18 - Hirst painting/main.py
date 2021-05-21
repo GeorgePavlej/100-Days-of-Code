4cdc0244d8946b2c0b1d8301622910c21a334022
Gyorgy1988 <80534232+Gyorgy1988@users.noreply.github.com>
2021-05-21 14:08:15 +0100

    Add files via upload

---
+++ Day 18 - Hirst painting/main.py
@@ -0,0 +1,30 @@
+import turtle
+from turtle import Turtle, Screen
+import random
+
+timmy = Turtle()
+turtle.colormode(255)
+color_list = [(236, 35, 108), (221, 232, 237), (145, 28, 64), (239, 75, 35), (6, 148, 93), (232, 238, 234), (231, 168, 40), (184, 158, 46), (44, 191, 233), (27, 127, 195), (126, 193, 74), (253, 223, 0), (85, 28, 93), (173, 36, 97), (246, 219, 44), (44, 172, 112), (215, 130, 165), (215, 56, 27)]
+
+timmy.speed(10)
+timmy.hideturtle()
+timmy.penup()
+timmy.setheading(225)
+timmy.forward(300)
+timmy.setheading(0)
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+    timmy.dot(20, random.choice(color_list))
+    timmy.forward(50)
+
+    if dot_count % 10 == 0:
+        timmy.setheading(90)
+        timmy.forward(50)
+        timmy.setheading(180)
+        timmy.forward(500)
+        timmy.setheading(0)
+
+        
+screen = Screen()
+screen.exitonclick()
