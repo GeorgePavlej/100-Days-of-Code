c69796dd567bce2ab4ffe195f56b947f2f1aeffa
Gyorgy1988 <80534232+Gyorgy1988@users.noreply.github.com>
2021-06-05 15:19:04 +0100

    Add files via upload

---
+++ Day 23 - Turtle Race/main.py
@@ -0,0 +1,44 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.title("Turtle Race")
+screen.tracer(0)
+
+car_manager = CarManager()
+player = Player()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # Detect collision with a car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    # Detect players collision with a wall
+    if player.ycor() > 280:
+        player.finish_line()
+        car_manager.level_up()
+        scoreboard.point_up()
+
+    # Update scoreboard
+
+
+
+screen.exitonclick()
