27183e8e125a9702aa0eacab83acd85c6f55a037
Gyorgy1988 <80534232+Gyorgy1988@users.noreply.github.com>
2021-07-05 17:22:20 +0100

    Update main.py

--- Day 29 - Password manager/main.py
+++ Day 29 - Password manager/main.py
@@ -1,13 +1,14 @@
 from tkinter import *
 from tkinter import messagebox
 import random
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    numbers = ['0', '1  ', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
     password_letters = [random.choice(letters) for _ in range(random.randint(8, 10))]
@@ -20,26 +21,62 @@ def generate_password():
     password = "".join(password_list)
     entry_pass_label.insert(0, password)
 
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save():
+
     website = entry_web_label.get()
     email = entry_email_label.get()
     password = entry_pass_label.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
-        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
+        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"The are the details entered \nEmail: {email}"
-                                                f"\nPassword: {password} \nIs it ok to save?")
-        if is_ok:
-            with open ("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                entry_web_label.delete(0, END)
-                entry_pass_label.delete(0, END)
+        try:
+            with open("data.json", "r") as data_file:
+                #Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            #Updating old data with new data
+            data.update(new_data)
+
+            with open("data.json", "w") as data_file:
+                #Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
+            entry_web_label.delete(0, END)
+            entry_pass_label.delete(0, END)
+
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+
+def find_password():
+    website = entry_web_label.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data file found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"Sorry, the website {website} is not saved!")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
-
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
@@ -77,6 +114,9 @@ entry_pass_label.grid(row=3, column=1, sticky="EW")
 
 gen_pass_button = Button(text="Generate Password", command=generate_password)
 gen_pass_button.grid(row=3, column=2)
+
+search_button = Button(text="Search", command=find_password)
+search_button.grid(row=1, column=2, columnspan=2, sticky="EW")
 
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
