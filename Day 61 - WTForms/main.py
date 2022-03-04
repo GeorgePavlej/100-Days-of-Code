7e528a66f8dd52db280e987acadbcb05379c556f
Gyorgy1988 <80534232+Gyorgy1988@users.noreply.github.com>
2022-03-04 21:25:47 +0000

    Add files via upload

---
+++ Day 61 - WTForms/main.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
+from flask_bootstrap import Bootstrap
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField('Sign In')
+
+
+app = Flask(__name__)
+app.secret_key = "13570530"
+Bootstrap(app)
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "georgepavlej@gmail.com" and form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template('login.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
